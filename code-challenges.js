91233ed62f192b04d198d772eba97c52ff25f998
Sarah Proctor <sproctor950@gmail.com>
2020-02-14 11:40:39 -0800

    updates week 2 assessment

---
+++ code-challenges.js
@@ -0,0 +1,64 @@
+// ASSESSMENT 2: Coding Practical Questions
+
+// --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
+// Use the test variables provided.
+
+var num1 = 15
+// Expected output: "15 is divisible by three"
+
+var num2 = 0
+// Expected output: "0 is divisible by three"
+
+var num3 = -7
+// Expected output: "-7 is not divisble by three"
+
+
+
+
+
+// --------------------2) Create a function that takes in the randomNouns variable and returns an array with all the words capitalized.
+
+var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
+// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
+
+
+
+
+
+// --------------------3) Create a function that takes in the mixedDataArray array and returns an array with ONLY NUMBERS sorted from least to greatest.
+
+var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
+// Expected output: [-8, 0, 8, 46, 59, 90, 107]
+
+
+
+
+
+// --------------------4) Create a function that takes in a string and logs the index of the first vowel.
+// Use the test variables provided.
+
+var vowelTester1 = "learn"
+// Expected output: 1
+var vowelTester1 = "throw"
+// Expected output: 3
+
+
+
+
+
+
+// --------------------5) Create a function that takes three arguments - two numbers and a mathematical operation (+, -, *, /) and performs the given calculation. If the input tries to divide by 0, return: "Can't divide by 0!"
+
+
+
+
+
+// Uncomment and use the following console logs to test your function
+// console.log(calculator(3, "*" 9))
+// Expected output: 27
+
+// console.log(calculator(16, "+" 3))
+// Expected output: 19
+
+// console.log(calculator(89, "/" 0))
+// Expected output: "Can't divide by 0!"
---
+++ interview-questions.md
@@ -0,0 +1,60 @@
+# ASSESSMENT 2: Interview Practice Questions
+
+Answer the following questions. First, without external resources. Challenge yourself to answer from memory. Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn.
+
+1. What does CRUD stand for?
+
+  Your answer:
+
+  Researched answer:
+
+
+
+2. What are the 5 HTTP verbs?
+
+  Your answer:
+
+  Researched answer:
+
+
+
+3. When creating a basic (stateless) class component in React, what are the necessary elements needed to render "Hello World" on the page?
+
+  Your answer:
+
+  Researched answer:
+
+
+
+4. What is JSX?
+
+  Your answer:
+
+  Researched answer:
+
+
+
+5. What is the difference between React state and props?
+
+  Your answer:
+
+  Researched answer:
+
+
+
+6. STRETCH: What is hoisting in JavaScript?
+
+  Your answer:
+
+  Researched answer:
+
+
+
+## Looking Ahead: Terms for Next Week
+
+Research and define the following terms to the best of your ability.
+
+- React lifecycle methods
+- API
+- event.preventDefault()
+- DOM events
